032eea4cb5b1e7707353dff1056cf7e8d16eaace
Julien-LAPORTE <samneo.jl@gmail.com>
2023-03-20 23:19:02 +0100

    - Activation feature View Binding et refactoring de MainActivity avec cette feature

--- app/src/main/java/fr/samneo/android/controller/MainActivity.java
+++ app/src/main/java/fr/samneo/android/controller/MainActivity.java
@@ -1,7 +1,5 @@
 package fr.samneo.android.controller;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,17 +9,12 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import fr.samneo.android.R;
+import fr.samneo.android.databinding.ActivityMainBinding;
 import fr.samneo.android.model.User;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView m_greetingTextView;
-    private EditText m_nameEditText;
-    private Button m_playButton;
+    private ActivityMainBinding m_binding;
     private User m_user;
     private static final int GAME_ACTIVITY_REQUEST_CODE = 42;
     private static final String SHARED_PREF_USER_INFO = "SHARED_PREF_USER_INFO";
@@ -45,26 +38,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        m_binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = m_binding.getRoot();
+        setContentView(view);
 
-        m_greetingTextView = findViewById(R.id.main_textview_greeting);
-        m_nameEditText = findViewById(R.id.main_edittext_name);
-        m_playButton = findViewById(R.id.main_button_play);
         m_user = new User();
         SharedPreferences preferences = getSharedPreferences(SHARED_PREF_USER_INFO, MODE_PRIVATE);
         String tempFirstName = preferences.getString(SHARED_PREF_USER_INFO_NAME, null);
         int tempScore = preferences.getInt(SHARED_PREF_USER_INFO_SCORE, 0);
         if (tempFirstName != null) {
-            m_nameEditText.setText(tempFirstName);
-            m_nameEditText.setSelection(tempFirstName.length());
-            m_greetingTextView
+            m_binding.mainEdittextName.setText(tempFirstName);
+            m_binding.mainEdittextName.setSelection(tempFirstName.length());
+            m_binding.mainTextviewGreeting
                     .setText("Bon retour " + tempFirstName +
                             "\n\nVotre score précédent était de : " + tempScore);
         } else {
-            m_playButton.setEnabled(false);
+            m_binding.mainButtonPlay.setEnabled(false);
         }
 
-        m_nameEditText.addTextChangedListener(new TextWatcher() {
+        m_binding.mainEdittextName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -77,13 +69,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                m_playButton.setEnabled(!s.toString().isEmpty());//Si pas vide on active le bouton
+                m_binding.mainButtonPlay.setEnabled(!s.toString().isEmpty());//Si pas vide on active le bouton
             }
         });
-        m_playButton.setOnClickListener(new View.OnClickListener() {
+        m_binding.mainButtonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                m_user.setFirstName(m_nameEditText.getText().toString());
+                m_user.setFirstName(m_binding.mainEdittextName.getText().toString());
                 //TODO: Formule non deprecated à étudier
                 /*ActivityResultLauncher<Intent> startActivityForResult = registerForActivityResult(
                         new ActivityResultContracts.StartActivityForResult(),
